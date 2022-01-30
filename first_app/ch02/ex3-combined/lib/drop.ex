1be7714106d34a0420fbe5ecd861e44461d183ce
Edelita Valdez <edaleeta@gmail.com>
2022-01-30 15:51:04 -0800

    Completes example 3-8

--- first_app/ch02/ex3-combined/lib/drop.ex
+++ first_app/ch02/ex3-combined/lib/drop.ex
@@ -3,16 +3,21 @@ defmodule Drop do
   Functions calculating velocities achieved by objects dropped in a vacuum.
   """
   import :math, only: [sqrt: 1]
-  
-  def fall_velocity({:earth, distance}) when distance >= 0 do
+
+  def fall_velocity(where) do
+    {planemo, distance} = where
+    fall_velocity(planemo, distance)
+  end
+
+  defp fall_velocity(:earth, distance) when distance >= 0 do
     sqrt(2 * 9.8 * distance)
   end
 
-  def fall_velocity({:moon, distance}) when distance >= 0 do
+  defp fall_velocity(:moon, distance) when distance >= 0 do
     sqrt(2 * 1.6 * distance)
   end
 
-  def fall_velocity({:mars, distance}) when distance >= 0 do
+  defp fall_velocity(:mars, distance) when distance >= 0 do
     sqrt(2 * 3.71 * distance)
   end
 end

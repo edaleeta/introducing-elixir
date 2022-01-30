9dd5ecbe4527b780f338c8b7bf0b2813c40d413e
Edelita Valdez <edaleeta@gmail.com>
2022-01-30 15:23:07 -0800

    Completes etude 2-3

--- etudes/lib/geom.ex
+++ etudes/lib/geom.ex
@@ -1,6 +1,13 @@
 defmodule Geom do
-  @moduledoc false
+  @moduledoc """
+  Includes geometric calculations
+  """
 
+  @doc """
+  Calculates the area of a rectangle with a given length and width
+  The default value for both arguments is 1
+  """
+  @spec area(number(), number()) :: number()
   def area(length \\ 1, width \\ 1) when length > 0 and width > 0 do
     length * width
   end

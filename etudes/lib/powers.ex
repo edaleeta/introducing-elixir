aca08ea6fc15a0584fb0d19292e6774219f74620
Edelita Valdez <edaleeta@gmail.com>
2022-01-30 19:05:47 -0800

    Completes etude 4-4

--- etudes/lib/powers.ex
+++ etudes/lib/powers.ex
@@ -1,5 +1,5 @@
 defmodule Powers do
-  import Kernel, except: [raise: 2]
+  import Kernel, except: [raise: 2, raise: 3]
 
   @moduledoc false
 
@@ -18,11 +18,19 @@ defmodule Powers do
   end
 
   def raise(x, n) when n > 0 do
-    x * raise(x, n-1)
+    raise(x, n, 1)
   end
 
   def raise(x, n) when n < 0 do
     1.0 / raise(x, -n)
+  end
+
+  def raise(_x, n, accumulator) when n == 0 do
+    accumulator
+  end
+
+  def raise(x, n, accumulator) do
+    raise(x, n-1, x * accumulator)
   end
 
 end

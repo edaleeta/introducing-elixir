e80637d0646db95b6cd94fccd640bed970984881
Edelita Valdez <edaleeta@gmail.com>
2022-01-30 19:23:31 -0800

    Completes etude 4-5

--- etudes/lib/powers.ex
+++ etudes/lib/powers.ex
@@ -33,4 +33,29 @@ defmodule Powers do
     raise(x, n-1, x * accumulator)
   end
 
+  @doc """
+  Calculate the nth root of x using the Newton-Raphson method
+  """
+
+  @spec nth_root(number(), number()) :: number()
+
+  def nth_root(x, n) do
+    nth_root(x, n, x / 2.0)
+  end
+
+  def nth_root(x, n, a) do
+    IO.puts("Current guess is #{a}")
+    f = raise(a, n) - x
+    f_prime = n * raise(a, n-1)
+    next = a - f / f_prime
+    change = abs(next - a)
+
+    if change < 1.0e-8 do
+      next
+    else
+      nth_root(x, n, next)
+    end
+
+  end
+
 end

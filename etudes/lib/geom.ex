44abbe7c76b386e0ac9018154502becd1ad71c4d
Edelita Valdez <edaleeta@gmail.com>
2022-01-30 16:15:21 -0800

    Completes etudes 3-1, 3-2

--- etudes/lib/geom.ex
+++ etudes/lib/geom.ex
@@ -3,13 +3,30 @@ defmodule Geom do
   Includes geometric calculations
   """
 
+  @type shape :: :ellipse | :rectangle | :triangle
+
   @doc """
-  Calculates the area of a rectangle with a given length and width
-  The default value for both arguments is 1
+  Calculates the area of an ellipse, rectangle, or triangle
   """
-  @spec area(number(), number()) :: number()
-  def area(length \\ 1, width \\ 1) when length > 0 and width > 0 do
+  @spec area({shape, number(), number()}) :: number()
+  def area({shape, a, b}) do
+    area(shape, a, b)
+  end
+
+  @spec area(:rectangle, number(), number()) :: number()
+  defp area(:rectangle, length, width) when length > 0 and width > 0 do
     length * width
   end
+
+  @spec area(:triangle, number(), number()) :: number()
+  defp area(:triangle, base, height) when base > 0 and height > 0 do
+    base * height / 2.0
+  end
+
+  @spec area(:ellipse, number(), number()) :: number()
+  defp area(:ellipse, major_radius, minor_radius) when major_radius > 0 and minor_radius > 0 do
+    :math.pi() * major_radius * minor_radius
+  end
+
 
 end

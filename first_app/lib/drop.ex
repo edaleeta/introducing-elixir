8508bd6bfaf869a955f40a5d50a40d901a2643c4
Edelita Valdez <edaleeta@gmail.com>
2022-01-30 17:17:50 -0800

    Completes chapter 4 examples

--- first_app/lib/drop.ex
+++ first_app/lib/drop.ex
@@ -4,21 +4,27 @@ defmodule Drop do
   """
   import :math, only: [sqrt: 1]
 
-  def fall_velocity(where) do
-    {planemo, distance} = where
-    fall_velocity(planemo, distance)
+  def fall_velocity(planemo, distance) when distance >= 0 do
+    gravity = case planemo do
+      :earth -> 9.8
+      :moon -> 1.6
+      :mars -> 3.71
+    end
+    velocity = sqrt(2 * gravity * distance)
+
+    if velocity > 20 do
+      IO.puts("Look out below!")
+    else
+      IO.puts("Reasonable...")
+    end
+
+    cond do
+      velocity == 0 -> :stable
+      velocity < 5 -> :slow
+      velocity >= 5 and velocity < 10 -> :moving
+      velocity >= 10 and velocity < 20 -> :fast
+      velocity >= 20 -> :speedy
+    end
   end
 
-  defp fall_velocity(:earth, distance) when distance >= 0 do
-    sqrt(2 * 9.8 * distance)
-  end
-
-  defp fall_velocity(:moon, distance) when distance >= 0 do
-    sqrt(2 * 1.6 * distance)
-  end
-
-  defp fall_velocity(:mars, distance) when distance >= 0 do
-    sqrt(2 * 3.71 * distance)
-  end
 end
-
---
+++ first_app/lib/fact.ex
@@ -0,0 +1,33 @@
+defmodule Fact do
+  @moduledoc false
+
+#  def factorial(n) when n > 1 do
+#    IO.puts("Calling from #{n}")
+#    result = n * factorial(n - 1)
+#    IO.puts("#{n} yields #{result}")
+#    result
+#  end
+#
+#  def factorial(n) when n <= 1 do
+#    IO.puts("Calling from #{n}")
+#    IO.puts("#{n} yields 1")
+#    1
+#  end
+
+# Tail recursive
+  def factorial(n) do
+    factorial(1, n, 1)
+  end
+
+  defp factorial(current, n, result) when current <= n do
+    new_result = result * current
+    IO.puts("#{current} yields #{new_result}")
+    factorial(current + 1, n, new_result)
+  end
+
+  defp factorial(_current, _n, result) do
+    IO.puts("finished")
+    result
+  end
+
+end
